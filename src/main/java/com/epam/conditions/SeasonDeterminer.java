e2468bf913ee0250b66146f8bf1ba38bf9763eb2
Utkirbek Qutfiddinov <utkirbek_qutfiddinov@mail.ru>
2023-11-07 20:44:51 +0500

    SeasonDeterminer task has been completed

--- src/main/java/com/epam/conditions/SeasonDeterminer.java
+++ src/main/java/com/epam/conditions/SeasonDeterminer.java
@@ -3,6 +3,19 @@ package com.epam.conditions;
 public class SeasonDeterminer {
 
     public void tellTheSeason(int monthNumber) {
+
+        switch (monthNumber){
+            case 1:
+                System.out.println("Winter"); break;
+            case 2:
+                System.out.println("Spring"); break;
+            case 3:
+                System.out.println("Summer"); break;
+            case 4:
+                System.out.println("Autumn"); break;
+            default:
+                System.out.println("Wrong month number");
+        }
     }
 
 }
