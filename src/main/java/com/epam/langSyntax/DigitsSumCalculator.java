a33edfa48ec97807e1c3c4c3677e1ce2e7e33890
Utkirbek Qutfiddinov <utkirbek_qutfiddinov@mail.ru>
2023-11-07 20:39:12 +0500

    LangSyntax task has been completed

--- src/main/java/com/epam/langSyntax/DigitsSumCalculator.java
+++ src/main/java/com/epam/langSyntax/DigitsSumCalculator.java
@@ -3,6 +3,14 @@ package com.epam.langSyntax;
 public class DigitsSumCalculator {
 
     public void calculateSum(int number) {
+        int result=0;
+
+        while(number>0){
+            result+=number%10;
+            number/=10;
+        }
+
+        System.out.println(result);
     }
 
 }
--- src/main/java/com/epam/langSyntax/NumberReverter.java
+++ src/main/java/com/epam/langSyntax/NumberReverter.java
@@ -3,6 +3,13 @@ package com.epam.langSyntax;
 public class NumberReverter {
 
     public void revert(int number) {
+        int hundreds=number/100;
+        int tens=number/10%10;
+        int ones=number%10;
+
+        int result = ones*100+tens*10+hundreds;
+
+        System.out.println(result);
     }
 
 }
