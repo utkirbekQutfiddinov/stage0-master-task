acbf0c9ebf36af73a3f2ef82b6f0ae0a84b9ec54
Utkirbek Qutfiddinov <utkirbek_qutfiddinov@mail.ru>
2023-11-07 20:33:38 +0500

    OOP task has been completed

--- src/main/java/com/epam/OOP/Animal.java
+++ src/main/java/com/epam/OOP/Animal.java
@@ -1,5 +1,33 @@
 package com.epam.OOP;
 
 public class Animal {
+    private String color;
+    private int numberOfPaws;
+    private boolean hasFur;
 
+    public Animal(String color, int numberOfPaws, boolean hasFur) {
+        this.color = color;
+        this.numberOfPaws = numberOfPaws;
+        this.hasFur = hasFur;
+    }
+
+    public String getDescription(){
+        return "This animal is mostly "+this.color
+                +". It has "+getPaws()+" and "+(isHasFur()?"a":"no")+" fur.";
+    }
+
+    private String getPaws(){
+        if(this.numberOfPaws==0){
+            return "no";
+        }
+        if(this.numberOfPaws==1){
+            return "a paw";
+        }else{
+            return numberOfPaws+" paws";
+        }
+    }
+
+    public boolean isHasFur() {
+        return hasFur;
+    }
 }
