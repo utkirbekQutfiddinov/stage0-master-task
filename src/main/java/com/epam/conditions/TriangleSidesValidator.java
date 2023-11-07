38e30bf17198e0acfcd3845b3f55f215c2835a82
Utkirbek Qutfiddinov <utkirbek_qutfiddinov@mail.ru>
2023-11-07 20:49:40 +0500

    TriangleSidesValidator task has been completed

--- src/main/java/com/epam/conditions/TriangleSidesValidator.java
+++ src/main/java/com/epam/conditions/TriangleSidesValidator.java
@@ -2,8 +2,11 @@ package com.epam.conditions;
 
 public class TriangleSidesValidator {
 
-    public void validate(double firstSide, double secondSide, double thirdSide) {
-
-    }
-
+        public void validate(double firstSide, double secondSide, double thirdSide) {
+            if (firstSide + secondSide > thirdSide && firstSide + thirdSide > secondSide && secondSide + thirdSide > firstSide) {
+                System.out.println("this is a valid triangl");
+            } else {
+                System.out.println("it's not a triangle");
+            }
+        }
 }
