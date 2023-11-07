66f1415c4a06cd7b855bb1ce5129cc64713086a0
Utkirbek Qutfiddinov <utkirbek_qutfiddinov@mail.ru>
2023-11-07 20:51:29 +0500

    DaysInMonth task has been completed

--- src/main/java/com/epam/conditions/DaysInMonth.java
+++ src/main/java/com/epam/conditions/DaysInMonth.java
@@ -3,7 +3,33 @@ package com.epam.conditions;
 public class DaysInMonth {
 
     public void printDays(int year, int month) {
-
+        if (year < 0) {
+            System.out.println("invalid date");
+            return;
+        }
+        if (month < 1 || month > 12) {
+            System.out.println("invalid date");
+            return;
+        }
+        int daysInMonth;
+        switch (month) {
+            case 2:
+                if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) {
+                    daysInMonth = 29;
+                } else {
+                    daysInMonth = 28;
+                }
+                break;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                daysInMonth = 30;
+                break;
+            default:
+                daysInMonth = 31;
+                break;
+        }
+        System.out.println(daysInMonth);
     }
-
 }
